48b64e9b5a58226d849f26634606bdecbe132bbf
Will <commanderwill2142@gmail.com>
2023-02-10 23:26:23 -0700

    defined object-validator.js, updated tests to be better

--- object-validator.js
+++ object-validator.js
@@ -1,3 +1,3 @@
-export function validateKey() {
-
+export function validateKey(obj, key) {
+    return obj.hasOwnProperty(key);
 }
--- object-validator.test.js
+++ object-validator.test.js
@@ -7,25 +7,19 @@ import {validateKey} from "./object-validator";
 
 describe("#validateKey", () => {
     it("should return true if the key is present in the object", () => {
-        const obj = { name: "John Doe", age: 30 };
+        const obj = {name: "John Doe", age: 30};
         const key = "name";
         expect(validateKey(obj, key)).toEqual(true);
     });
 
     it("should return false if the key is not present in the object", () => {
-        const obj = { name: "John Doe", age: 30 };
+        const obj = {name: "John Doe", age: 30};
         const key = "address";
         expect(validateKey(obj, key)).toEqual(false);
     });
 
-    it("should return true if the key is present in the object with a different case", () => {
-        const obj = { name: "John Doe", age: 30 };
-        const key = "NAME";
-        expect(validateKey(obj, key)).toEqual(true);
-    });
-
     it("should return false if the key is a number", () => {
-        const obj = { name: "John Doe", age: 30 };
+        const obj = {name: "John Doe", age: 30};
         const key = 123;
         expect(validateKey(obj, key)).toEqual(false);
     });
