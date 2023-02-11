9fc50ff8088af7191dfc74ee57f3f0cd2f0a6681
Will <commanderwill2142@gmail.com>
2023-02-10 23:40:58 -0700

    noticed the tests were supposed to be typechecked, RIP. added type checking to numbers.test.js

--- numbers.test.js
+++ numbers.test.js
@@ -7,26 +7,43 @@ import {sumArray} from "./numbers";
 describe("#sumArray", () => {
     it("should return the sum of all numbers in the array", () => {
         const numbers = [1, 2, 3];
-        expect(sumArray(numbers)).toEqual(6);
+        const result = sumArray(numbers);
+        expect(result).toBeOfType("number");
+        expect(result).toEqual(6);
     });
 
     it("should return 0 if the array is empty", () => {
         const numbers = [];
-        expect(sumArray(numbers)).toEqual(0);
+        const result = sumArray(numbers);
+        expect(result).toBeOfType("number");
+        expect(result).toEqual(0);
     });
 
     it("should return the sum of all positive and negative numbers in the array", () => {
         const numbers = [-1, 2, 3, -4];
-        expect(sumArray(numbers)).toEqual(0);
+        const result = sumArray(numbers);
+        expect(result).toBeOfType("number");
+        expect(result).toEqual(0);
     });
 
-    // RIP floating points. Added in tolerances
     it("should return the sum of all decimal numbers in the array", () => {
         const numbers = [0.1, 0.2, 0.3];
         const expected = 0.6;
         const tolerance = 0.0000001;
         const result = sumArray(numbers);
+        expect(result).toBeOfType("number");
         expect(Math.abs(result - expected)).toBeLessThan(tolerance);
     });
 
+    it("should return NaN if the input is not an array", () => {
+        const numbers = "not an array";
+        const result = sumArray(numbers);
+        expect(result).toBeNaN();
+    });
+
+    it("should return NaN if the array contains non-numeric values", () => {
+        const numbers = [1, 2, "not a number"];
+        const result = sumArray(numbers);
+        expect(result).toBeNaN();
+    });
 });
